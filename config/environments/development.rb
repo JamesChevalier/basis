b428bd06f5e7e671a8b66a85bdac2c0c1c5d43eb
James Chevalier <jchevalier@gmail.com>
2013-02-26 19:04:23 -0500

    use mailchecker in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,22 @@ Basis::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'basis.dev' }
+
+  # Swap the 'config.action_mailer.smtp_settings' line commenting to use gmail
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'basis.com',
+  #   :user_name            => "",
+  #   :password             => "",
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true
+  # }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
