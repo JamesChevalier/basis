d9bfe2cfd3ab8e48f363676b939b5bedb2e3766d
James Chevalier <jchevalier@gmail.com>
2013-03-02 13:19:19 -0500

    comment unused paperclip config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,8 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
-  require 'paperclip/matchers'
+  # Uncomment the following line if you're using Paperclip
+  # require 'paperclip/matchers'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
