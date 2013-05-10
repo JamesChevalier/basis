97ebf19753a19ff9c5397e88e8778cd39fc0f008
James Chevalier <jchevalier@gmail.com>
2013-05-10 15:42:10 -0400

    add users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # The complete list of Devise modules is available at https://github.com/plataformatec/devise
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
